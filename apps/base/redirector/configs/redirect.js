82a8c46df7b8dd9aa7d75033b014e37edd04d73d
Nick Osborn <nosborn@users.noreply.github.com>
2026-02-21 18:55:33 +0000

    redirector: prefer 307

--- apps/base/redirector/configs/redirect.js
+++ apps/base/redirector/configs/redirect.js
@@ -1,7 +1,7 @@
 function redirectToUrlParameter(r) {
   var url = r.args.url;
   if (url) {
-    r.return(302, decodeURIComponent(url));
+    r.return(307, decodeURIComponent(url));
   } else {
     r.return(400);
   }
