868385323fb39fe2b7b0ca4b1feb20ca468f80b7
Nick Osborn <nosborn@users.noreply.github.com>
2026-02-21 10:40:55 +0000

    Enable kiali and redirector apps

---
+++ apps/base/redirector/configs/redirect.js
@@ -0,0 +1,10 @@
+function redirectToUrlParameter(r) {
+  var url = r.args.url;
+  if (url) {
+    r.return(302, decodeURIComponent(url));
+  } else {
+    r.return(400);
+  }
+}
+
+export default { redirectToUrlParameter };
